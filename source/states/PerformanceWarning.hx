9c37bfc92ef424d0b533c3b6c142b9b9babd296c
MrMadera <ismaelglhs@gmail.com>
2025-03-21 07:38:00 +0000

    performance screen

---
+++ source/states/PerformanceWarning.hx
@@ -0,0 +1,84 @@
+package states;
+
+import flixel.FlxSubState;
+
+import flixel.effects.FlxFlicker;
+import lime.app.Application;
+
+class PerformanceWarning extends MusicBeatState
+{
+	public static var leftState:Bool = false;
+
+	var isYes:Bool = true;
+	var texts:FlxTypedSpriteGroup<FlxText>;
+	var bg:FlxSprite;
+
+	override function create()
+	{
+		super.create();
+
+		bg = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
+		add(bg);
+
+		texts = new FlxTypedSpriteGroup<FlxText>();
+		texts.alpha = 0.0;
+		add(texts);
+
+		var warnText:FlxText = new FlxText(0, 0, FlxG.width,
+			"Warning!\n
+			This Mod may have memory leaks and severe lags if you have a low-end pc\n
+			We recommend you to activate the low quality setting in the options menu to avoid them");
+		warnText.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER);
+		warnText.screenCenter(Y);
+		texts.add(warnText);
+
+		final keys = ["I understand"];
+		for (i in 0...keys.length) {
+			final button = new FlxText(0, 0, FlxG.width, keys[i]);
+			button.setFormat(Paths.font("vcr.ttf"), 32, FlxColor.WHITE, CENTER);
+			button.y = (warnText.y + warnText.height) + 44;
+			//button.x += (128 * i) - 80;
+            button.screenCenter(X);
+			texts.add(button);
+		}
+
+		FlxTween.tween(texts, {alpha: 1.0}, 0.5, {
+			onComplete: (_) -> updateItems()
+		});
+	}
+
+	override function update(elapsed:Float)
+	{
+		if(leftState) {
+			super.update(elapsed);
+			return;
+		}
+		var back:Bool = controls.BACK;
+		if (controls.UI_LEFT_P || controls.UI_RIGHT_P) {
+			FlxG.sound.play(Paths.sound("scrollMenu"), 0.7);
+			isYes = !isYes;
+			updateItems();
+		}
+		if (controls.ACCEPT) {
+			leftState = true;
+			FlxTransitionableState.skipNextTransIn = true;
+			FlxTransitionableState.skipNextTransOut = true;
+			FlxG.sound.play(Paths.sound('confirmMenu'));
+			final button = texts.members[isYes ? 1 : 2];
+			FlxFlicker.flicker(button, 1, 0.1, false, true, function(flk:FlxFlicker) {
+				new FlxTimer().start(0.5, function (tmr:FlxTimer) {
+					FlxTween.tween(texts, {alpha: 0}, 0.2, {
+						onComplete: (_) -> MusicBeatState.switchState(new TitleState())
+					});
+				});
+			});
+			
+		}
+		super.update(elapsed);
+	}
+
+	function updateItems() {
+		// it's clunky but it works.
+		texts.members[1].alpha = isYes ? 1.0 : 0.6;
+	}
+}
