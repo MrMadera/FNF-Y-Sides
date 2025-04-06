f1aa191b2978a17975322c8d372aba0674fefe8f
MrMadera <ismaelglhs@gmail.com>
2025-04-06 14:51:50 +0100

    song events

--- assets/shared/custom_events/smooth_camera_zoom.lua
+++ assets/shared/custom_events/smooth_camera_zoom.lua
@@ -2,5 +2,6 @@ function onEvent(name, value1, value2, strumTime)
     if name == 'smooth_camera_zoom' then
         local value_2 = stringSplit(value2, ",")
         doTweenZoom('zoomTween', 'game', value1, value_2[1], value_2[2]);
+        setProperty('defaultCamZoom', value1)
     end
 end
